cbadda611b780eaca38edbdf19fbbfea2467549a
seomi9516 <seomi9516@gmail.com>
2025-04-03 00:27:32 +0900

    feature: change favicon, router path and api url

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ import TestMap from "@/components/TestMap.vue";
 
 const routes = [
     {
-        path: "/test",  // TestComponent.vue로 이동할 Path
+        path: "/",  // TestComponent.vue로 이동할 Path
         name: "TestMap",  // router name
         component: TestMap,  // Path로 이동될 Component
     },
--- src/store/newsListStore.js
+++ src/store/newsListStore.js
@@ -3,12 +3,12 @@ import { ref } from 'vue'
 import axios from 'axios'
 
 export const useNewsListStore = defineStore('counter', () => {
-    // TODO 임시
     const articles = ref([])
     const filteredArticles = ref([])
 
     const loadArticles = async () => {
-      const res = await axios.get('/articles', { headers: {'Content-Type': 'application/json'} })
+      const url = process.env.VUE_APP_DB_SERVER + '/articles'
+      const res = await axios.get(url, { headers: {'Content-Type': 'application/json'} })
       articles.value = res.data
       articles.value.forEach((article) => { if (article.category === "") article.category = '기타' })
     }
