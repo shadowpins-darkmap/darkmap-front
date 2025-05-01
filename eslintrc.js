b24a8b7f38aa9eaa443ef8ea5cab8704d49ee898
constmoon <jiwoo.link@gmail.com>
2025-05-01 15:49:43 +0900

    린트 규칙 추가

---
+++ eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  rules: {
+    semi: ['error', 'always'],
+    indent: ['error', 2],
+  },
+}
