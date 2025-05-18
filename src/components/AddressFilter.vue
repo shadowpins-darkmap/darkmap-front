25de8c9b32dcba7300d908408e3b96c455ebaa50
constmoon <jiwoo.link@gmail.com>
2025-05-18 17:31:50 +0900

    드롭다운 및 스크롤바 UI 변경

--- src/components/AddressFilter.vue
+++ src/components/AddressFilter.vue
@@ -1,41 +1,30 @@
 <script setup>
-import { ref, defineEmits, defineProps } from "vue"
-import "@/styles/AddressFilter.scss"
+import { ref, defineEmits, defineProps } from 'vue';
+import '@/styles/AddressFilter.scss';
+import BaseDropdown from './BaseDropdown.vue';
 
 const props = defineProps({
   addressData: {
     type: Array,
     required: true,
   },
-})
+});
 
-const selectedGu = ref(props.addressData[0].lv1)
-const openDropdown = ref(false)
-const emit = defineEmits(["change"])
+const selectedGu = ref(props.addressData[0].lv1);
+const openDropdown = ref(false);
+const emit = defineEmits(['change']);
 
 const clickGu = (lv1) => {
-  selectedGu.value = lv1
-  openDropdown.value = false
-  emit("change", lv1)
-}
-
-const isLast = (idx) => idx === props.addressData.length - 1
+  selectedGu.value = lv1;
+  openDropdown.value = false;
+  emit('change', lv1);
+};
 </script>
 
 <template>
-  <div style="position: relative; margin-left: 8px">
-    <div class="dropdown" @click="() => (openDropdown = !openDropdown)">
-      <div class="dropdown_text">{{ selectedGu }}</div>
-      <img src="../assets/arrowDropDown.svg" />
-    </div>
-    <div class="dropdown-list" v-if="openDropdown">
-      <div v-for="(address, idx) in addressData" :key="idx">
-        <div
-          :class="['dropdown-item', isLast(idx) ? '' : 'not-last']"
-          @click="clickGu(address.lv1)">
-          {{ address.lv1 }}
-        </div>
-      </div>
-    </div>
-  </div>
+  <BaseDropdown
+    :list="addressData.map((item) => item.lv1)"
+    :onSelect="clickGu"
+    :selected="selectedGu"
+  />
 </template>
---
+++ src/components/BaseDropdown.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="BaseDropdown" ref="dropdownRef">
+    <button class="BaseDropdown__toggle" @click="toggleDropdown">
+      {{ selectedLabel }}
+    </button>
+    <div v-if="isOpen" class="BaseDropdown__menu">
+      <button class="BaseDropdown__selected" @click="toggleDropdown">
+        {{ selectedLabel }}
+      </button>
+      <ul class="BaseDropdown__menuList">
+        <li
+          v-for="(item, index) in list"
+          :key="index"
+          class="BaseDropdown__menuItem"
+          @click="handleSelect(item)"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted, defineProps, watch } from 'vue';
+
+const props = defineProps({
+  list: {
+    type: Array,
+    required: true,
+  },
+  selected: {
+    type: String,
+    default: '',
+  },
+  onSelect: {
+    type: Function,
+    required: true,
+  },
+});
+
+const isOpen = ref(false);
+const selectedLabel = ref(props.selected || props.list[0]);
+
+const toggleDropdown = () => {
+  isOpen.value = !isOpen.value;
+};
+
+const handleSelect = (item) => {
+  selectedLabel.value = item;
+  isOpen.value = false;
+  props.onSelect(item);
+};
+
+watch(
+  () => props.selected,
+  (newVal) => {
+    if (newVal !== selectedLabel.value) {
+      selectedLabel.value = newVal;
+    }
+  },
+);
+
+const dropdownRef = ref(null);
+const onClickOutside = (e) => {
+  if (!dropdownRef.value?.contains(e.target)) {
+    isOpen.value = false;
+  }
+};
+onMounted(() => window.addEventListener('mousedown', onClickOutside));
+onUnmounted(() => window.removeEventListener('mousedown', onClickOutside));
+</script>
+
+<style scoped lang="scss">
+.BaseDropdown {
+  position: relative;
+  display: inline-block;
+  color: #ffefeb;
+  font-weight: 700;
+  text-align: left;
+  font-size: 14px;
+
+  &__toggle {
+    min-width: 80px;
+    padding: 8px 8px 8px 10px;
+    text-align: left;
+    line-height: 1;
+    font-size: 14px;
+    font-weight: 600;
+    border: 2px solid #ffefeb;
+    border-radius: 16px;
+    margin-left: 8px;
+    background: transparent;
+    color: #ffefeb;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &::after {
+      margin-left: 8px;
+      content: '';
+      width: 8px;
+      height: 8px;
+      background-image: url('@/assets/arrowDropDown.svg');
+      background-repeat: no-repeat;
+      background-position: center;
+    }
+  }
+
+  &__menu {
+    position: absolute;
+    top: 0;
+    left: 8px;
+    background: #5966d8;
+    padding: 7px 10px;
+    border: 2px solid #ffefeb;
+    border-radius: 16px;
+    width: 146px;
+    height: auto;
+    max-height: 185px;
+    z-index: 10;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  }
+
+  &__selected {
+    width: 100%;
+    padding: 0;
+    background: inherit;
+    border: 0;
+    display: block;
+    text-align: left;
+    cursor: pointer;
+    font-weight: inherit;
+    color: inherit;
+    font-size: inherit;
+  }
+
+  &__menuList {
+    max-height: 155px;
+    overflow-y: auto;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    cursor: pointer;
+    margin-top: 4px;
+    border-top: 1px solid #fff;
+
+    &::-webkit-scrollbar {
+      width: 10px;
+      height: 10px;
+    }
+    &::-webkit-scrollbar:hover {
+      width: 10px;
+      height: 10px;
+    }
+    &::-webkit-scrollbar-button {
+      display: none;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: #ffefeb;
+      border-radius: 6px;
+    }
+    &::-webkit-scrollbar-track {
+      background-color: rgba(255, 239, 235, 0.65);
+      margin: 6px 0;
+      border-radius: 6px;
+    }
+  }
+
+  &__menuItem {
+    padding: 5px 0;
+    cursor: pointer;
+    &:first-child {
+      margin-top: 4px;
+    }
+  }
+}
+</style>
