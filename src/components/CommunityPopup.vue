98c3225c9ee3ff55897cbafabfc6a86cdc430199
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-06-13 12:30:51 +0900

    커뮤니티 팝업 템플릿생성

---
+++ src/components/CommunityPopup.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="BaseCommunity">
+    <section class="BaseCommunity__popup">
+			
+		</section>
+    <section class="BaseCommunity__popup"></section>
+  </div>
+</template>
+
+<script setup>
+// import { ref, computed, defineEmits, watch } from 'vue';
+// import { useDevice } from '@/composables/useDevice';
+
+// const { isMobile } = useDevice();
+</script>
+
+<style scoped lang="scss">
+.BaseCommunity {
+  position: absolute;
+  right: 40px;
+  top: 40px;
+  height: calc(100% - 80px);
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+
+  &__popup {
+    width: 436px;
+    padding: 25px;
+    border: solid 2px #f1cfc8;
+    background-color: #6d54ce;
+    border-radius: 20px;
+  }
+}
+</style>
--- src/components/RenderMap.vue
+++ src/components/RenderMap.vue
@@ -3,6 +3,7 @@
     <TestMap v-if="!isMobile" />
     <ControlMobile v-if="isMobile" />
     <ControlPopup v-else />
+    <CommunityPopup />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import TestMap from '@/components/TestMap.vue';
 import ControlPopup from '@/components/ControlPopup.vue';
 import ControlMobile from '@/components/ControlMobile.vue';
+import CommunityPopup from '@/components/CommunityPopup.vue';
 import { useDevice } from '@/composables/useDevice';
 
 const { isMobile } = useDevice();
