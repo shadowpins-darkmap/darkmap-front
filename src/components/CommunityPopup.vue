ef52c36d6e8ad9da939194358983edeb273b59f5
hyoseon <67184684+namoo93@users.noreply.github.com>
2025-06-19 13:01:52 +0900

    슬라이더 패널 컨텐츠 내용 넘칠시 스크롤

--- src/components/CommunityPopup.vue
+++ src/components/CommunityPopup.vue
@@ -148,20 +148,23 @@
             height="36"
           />
         </button>
-        <strong class="slider_title">길거리 괴롭힘이란게 뭔가요?</strong>
-
-        <p class="slider_contents">
-          용어로도 출판이나 연출을 지칭하는 입숨을 표준 이런 들어가는 디자인
-          결과물에 시각적 입숨은 그래픽 텍스트로, 텍스트로, 입숨은 분야에서
-          실제적인 보여줄 무언가를 표준 로렘 무언가를 때로 입숨은 채우기 입숨은
-          사용할 시각 이용된다.
-        </p>
-        <p class="slider_contents">
-          프로젝트 채우기 사용하는 차지하는 프로젝트 보여줄 내용이 입숨을
-          타이포그래피, 지칭하는 프로젝트 사용할 프로젝트 출판이나 채워지기
-          디자인 보여줄 프로젝트 내용이 차지하는 시각적 사용할 연출을
-          타이포그래피, 때
-        </p>
+        <strong class="slider_title ponit_color"
+          >길거리 괴롭힘이란게 뭔가요?</strong
+        >
+        <div class="slider_contents_wrap">
+          <p class="slider_contents">
+            용어로도 출판이나 연출을 지칭하는 입숨을 표준 이런 들어가는 디자인
+            결과물에 시각적 입숨은 그래픽 텍스트로, 텍스트로, 입숨은 분야에서
+            실제적인 보여줄 무언가를 표준 로렘 무언가를 때로 입숨은 채우기
+            입숨은 사용할 시각 이용된다.
+          </p>
+          <p class="slider_contents">
+            프로젝트 채우기 사용하는 차지하는 프로젝트 보여줄 내용이 입숨을
+            타이포그래피, 지칭하는 프로젝트 사용할 프로젝트 출판이나 채워지기
+            디자인 보여줄 프로젝트 내용이 차지하는 시각적 사용할 연출을
+            타이포그래피, 때
+          </p>
+        </div>
 
         <strong class="slider_title_sub">길거리 괴롭힘의 유형</strong>
         <ol class="slider_list">
@@ -232,6 +235,34 @@
             height="36"
           />
         </button>
+
+        <strong class="slider_title">1. 바바리맨</strong>
+        <div class="slider_contents_wrap">
+          <p class="slider_contents">
+            용어로도 출판이나 연출을 지칭하는 입숨을 표준 이런 들어가는 디자인
+            결과물에 시각적 입숨은 그래픽 텍스트로, 텍스트로, 입숨은 분야에서
+            실제적인 보여줄 무언가를 표준 로렘 무언가를 때로 입숨은 채우기
+            입숨은 사용할 시각 이용된다.
+          </p>
+          <p class="slider_contents">
+            프로젝트 채우기 사용하는 차지하는 프로젝트 보여줄 내용이 입숨을
+            타이포그래피, 지칭하는 프로젝트 사용할 프로젝트 출판이나 채워지기
+            디자인 보여줄 프로젝트 내용이 차지하는 시각적 사용할 연출을
+            타이포그래피, 때
+          </p>
+          <p class="slider_contents">
+            프로젝트 채우기 사용하는 차지하는 프로젝트 보여줄 내용이 입숨을
+            타이포그래피, 지칭하는 프로젝트 사용할 프로젝트 출판이나 채워지기
+            디자인 보여줄 프로젝트 내용이 차지하는 시각적 사용할 연출을
+            타이포그래피, 때
+          </p>
+          <p class="slider_contents">
+            프로젝트 채우기 사용하는 차지하는 프로젝트 보여줄 내용이 입숨을
+            타이포그래피, 지칭하는 프로젝트 사용할 프로젝트 출판이나 채워지기
+            디자인 보여줄 프로젝트 내용이 차지하는 시각적 사용할 연출을
+            타이포그래피, 때
+          </p>
+        </div>
       </div>
     </SlidePanel>
   </div>
@@ -500,18 +531,24 @@ onMounted(() => {
     justify-content: flex-end;
   }
   .slider_title {
-    color: #8270cb;
     font-weight: bold;
     font-size: 32px;
     display: flex;
     padding-top: 40px;
     padding-bottom: 60px;
   }
+  .ponit_color {
+    color: #8270cb;
+  }
   .slider_contents {
     padding-bottom: 25px;
     font-size: 16px;
     color: #fff;
     font-weight: normal;
+  }
+  .slider_contents_wrap {
+    overflow-y: auto;
+    max-height: 600px;
   }
   .slider_title_sub {
     color: #fff;
