7dfb2b9f7e337deae94e9611d5786ee0073007a1
Doori Kim <doori.alice.kim@gmail.com>
2026-01-27 23:10:58 +0900

    articles만 백엔드 url 타도록 변경

--- src/constant/url.js
+++ src/constant/url.js
@@ -3,3 +3,4 @@ export const BASE_URL = 'https://darkmap-front-five.vercel.app';
 
 export const IS_DEVELOPMENT = process.env.VUE_APP_IS_DEVELOPMENT === 'true';
 export const API_BASE_URL = '';
+export const DEFAULT_API_URL = 'https://api.kdark.weareshadowpins.com';
