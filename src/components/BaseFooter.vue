45b4551adbcda9541cc3e6ef0c745a74144a7ba5
Lee Seul <love.and.seul@gmail.com>
2026-02-22 17:54:47 +0900

    refactor: change privacy button to external Notion link

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -10,13 +10,14 @@
       >의 K-다크맵 투어 Ver.1.1
     </div>
     <div class="BaseFootercontainer">
-      <button
-        type="button"
+      <a
+        :href="'https://www.notion.so/20025ce1e4e980689f25e1db1373fd9c'"
+        target="_blank"
+        rel="noopener noreferrer"
         class="BaseFooterlink"
-        @click="isTermsPanelOpen = true"
       >
         개인정보처리방침
-      </button>
+      </a>
       <a
         :href="'https://forms.gle/Kr93YqK5ZjsiqvpF6'"
         target="_blank"
@@ -31,35 +32,14 @@
       >
     </div>
   </footer>
-
-  <!-- 사이트 이용약관 -->
-  <SlidePanel
-    :width="'510px'"
-    :visible="isTermsPanelOpen"
-    @close="isTermsPanelOpen = false"
-    :right="'auto'"
-    :left="'0'"
-  >
-    <TermsSidePanel
-      @close="isTermsPanelOpen = false"
-      @open-terms-panel="isTermsPanelOpen = true"
-    />
-  </SlidePanel>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-import SlidePanel from '@/components/slidePanel/SlidePanel.vue';
-import TermsSidePanel from '@/components/commonPanel/TermsSidePanel.vue';
-const isTermsPanelOpen = ref(false);
-</script>
 
 <style lang="scss" scoped>
 .BaseFooter {
   font-family: 'Roboto', sans-serif;
   font-size: 10px;
   font-weight: 500;
-  color: #FFEFEB;
+  color: #ffefeb;
   margin-top: 20px;
   padding-bottom: 5px;
 
@@ -75,20 +55,12 @@ const isTermsPanelOpen = ref(false);
     gap: 12px;
   }
 
-  &__link {
-    display: block;
-    text-decoration: underline;
-    color: #FFEFEB;
-  }
   .BaseFooterlink {
     font-family: 'Roboto', sans-serif;
     font-size: 10px;
     font-weight: 500;
-    color: #FFEFEB;
+    color: #ffefeb;
     text-decoration: underline;
-    background: none;
-    border: none;
-    padding: 0;
     cursor: pointer;
   }
 }
